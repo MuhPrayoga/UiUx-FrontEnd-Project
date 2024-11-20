fcd54af02a2ee495a5abe00d4bf12337ccaf2390
vriakbr <givari.akbar7@gmail.com>
2024-11-20 11:29:18 +0700

    update

--- Public/JS/script.js
+++ Public/JS/script.js
@@ -3,10 +3,10 @@ const description = document.getElementById('description');
 
 // Isi paragraf berdasarkan pilihan
 const descriptions = {
-    1: 'Penjelasan: Depresi adalah gangguan suasana hati yang menyebabkan perasaan sedih mendalam, kehilangan minat, dan kehilangan energi secara terus-menerus. Penderita depresi bisa merasa putus asa, sulit tidur, dan bahkan kehilangan minat pada aktivitas yang sebelumnya mereka nikmati. Kondisi ini juga bisa memengaruhi fungsi fisik, seperti selera makan dan energi. ',
-    2: 'Penjelasan: Bipolar adalah gangguan mood yang melibatkan episode perubahan suasana hati ekstrem, yaitu mania (perasaan euforia atau energi berlebihan) dan depresi, yang dapat memengaruhi perilaku, energi, dan kemampuan seseorang untuk menjalani kehidupan secara normal.',
-    3: 'Penjelasan: Skizofrenia adalah gangguan mental serius yang memengaruhi cara seseorang berpikir, merasakan, dan berperilaku, sering kali ditandai dengan gejala seperti delusi, halusinasi, dan kesulitan membedakan realitas dari fantasi.',
-    4: 'Penjelasan: Kecemasan adalah gangguan kesehatan mental yang ditandai dengan rasa khawatir atau takut yang berlebihan dan terus-menerus terhadap situasi atau hal tertentu, yang dapat disertai gejala fisik seperti detak jantung cepat, napas pendek, dan gangguan tidur.'
+    1: 'Depresi adalah gangguan suasana hati yang menyebabkan perasaan sedih mendalam, kehilangan minat, dan kehilangan energi secara terus-menerus. Penderita depresi bisa merasa putus asa, sulit tidur, dan bahkan kehilangan minat pada aktivitas yang sebelumnya mereka nikmati. Kondisi ini juga bisa memengaruhi fungsi fisik, seperti selera makan dan energi. ',
+    2: 'Bipolar adalah gangguan mood yang melibatkan episode perubahan suasana hati ekstrem, yaitu mania (perasaan euforia atau energi berlebihan) dan depresi, yang dapat memengaruhi perilaku, energi, dan kemampuan seseorang untuk menjalani kehidupan secara normal.',
+    3: 'Skizofrenia adalah gangguan mental serius yang memengaruhi cara seseorang berpikir, merasakan, dan berperilaku, sering kali ditandai dengan gejala seperti delusi, halusinasi, dan kesulitan membedakan realitas dari fantasi.',
+    4: 'Kecemasan adalah gangguan kesehatan mental yang ditandai dengan rasa khawatir atau takut yang berlebihan dan terus-menerus terhadap situasi atau hal tertentu, yang dapat disertai gejala fisik seperti detak jantung cepat, napas pendek, dan gangguan tidur.'
 };
 
 // Fungsi untuk memperbarui tampilan
