03c8df5c2a1fd7f1b4bbc8dd7ea09d0da3c766e4
518027 <rutt3644@gmail.com>
2024-11-24 11:49:29 +0700

    Login.js

---
+++ Public/JS/Login.js
@@ -0,0 +1,27 @@
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const username = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const user = users.find(user => user.username === username);
+    if (!user) {
+        errorShow("Username belum terdaftar!");
+        return;
+    }
+    if (user.password !== password) {
+        errorShow("Password salah!");
+        return;
+    }
+    alert("Login berhasil!");
+    window.location.href = "index.html";
+});
+
+function errorShow(message) {
+    const modal = document.getElementById('error-modal');
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = message;
+    modal.classList.remove('hidden');
+    document.getElementById('close-modal').addEventListener('click', function() {
+        modal.classList.add('hidden');
+    });
+}
